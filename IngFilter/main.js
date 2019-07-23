27a81bad845ae997a6f26751e626aae035ce867e
彭龙 <tccwpl@163.com>
2019-07-23 16:12:32 +0800

    添加刷新脚本-待完善

--- IngFilter/main.js
+++ IngFilter/main.js
@@ -5,7 +5,9 @@
         //加载页面时清除一次
         _this.ChearUsers();
         //此后每两秒定时清除一次
-        setInterval(_this.ChearUsers, 2000);
+        //setInterval(_this.ChearUsers, 2000);
+        //刷星
+        setTimeout(_this.Refresh, 1000*60*5);
     }
     this.ChearUsers = function () {
         var filterNames = new Array("测试人员01", "测试人员02", "测试人员03");
@@ -22,6 +24,40 @@
             });
         });
     }
+    this.Refresh=function(){
+        console.log("刷星~");
+        window.confirm = function (msg, callBack) {return true}
+        $("#txt_ing").val("_this.GetCurrTime()");
+        $("#btn_ing_publish").trigger("click")
+        $("#ing_my").trigger("click")
+        if (!$("#feed_list").find("ul").children().eq(0).find("[title='调皮星']")[0]||!$("#feed_list").find("ul").children().eq(0).find("[title='可爱星']")[0]) {
+            var id = $("#feed_list").find("ul").children().eq(0).find("#max_ing_id").html();
+            console.log(id)
+            $("#feed_list").find("ul").children().eq(0).find("[class='recycle']").trigger("click")
+        }
+    }
+    this.GetCurrTime=function(){
+        var date=new Date();
+        var year=date.getFullYear();
+        var month=date.getMonth()+1;
+        var day=date.getDate();
+        var hour=date.getHours();
+        var minute=date.getMinutes();
+        var second=date.getSeconds();
+        //这样写显示时间在1~9会挤占空间；所以要在1~9的数字前补零;
+        if (hour<10) {
+            hour='0'+hour;
+        }
+        if (minute<10) {
+            minute='0'+minute;
+        }
+        if (second<10) {
+            second='0'+second;
+        }
+        var x=date.getDay();//获取星期
+        return year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second 
+    }
 }
 var home = new Home();
 home.Init();
+
