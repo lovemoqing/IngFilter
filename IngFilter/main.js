e1398809f68b0fc58dd4cb508c1366162b2da700
默卿的Github <35390336+lovemoqing@users.noreply.github.com>
2020-05-29 21:27:55 +0800

    去掉无用的刷星脚本

--- IngFilter/main.js
+++ IngFilter/main.js
@@ -24,39 +24,6 @@
             });
         });
     }
-    this.Refresh=function(){
-        console.log("刷星~");
-        window.confirm = function (msg, callBack) {return true}
-        $("#txt_ing").val("_this.GetCurrTime()");
-        $("#btn_ing_publish").trigger("click")
-        $("#ing_my").trigger("click")
-        if (!$("#feed_list").find("ul").children().eq(0).find("[title='调皮星']")[0]||!$("#feed_list").find("ul").children().eq(0).find("[title='可爱星']")[0]) {
-            var id = $("#feed_list").find("ul").children().eq(0).find("#max_ing_id").html();
-            console.log(id)
-            $("#feed_list").find("ul").children().eq(0).find("[class='recycle']").trigger("click")
-        }
-    }
-    this.GetCurrTime=function(){
-        var date=new Date();
-        var year=date.getFullYear();
-        var month=date.getMonth()+1;
-        var day=date.getDate();
-        var hour=date.getHours();
-        var minute=date.getMinutes();
-        var second=date.getSeconds();
-        //这样写显示时间在1~9会挤占空间；所以要在1~9的数字前补零;
-        if (hour<10) {
-            hour='0'+hour;
-        }
-        if (minute<10) {
-            minute='0'+minute;
-        }
-        if (second<10) {
-            second='0'+second;
-        }
-        var x=date.getDay();//获取星期
-        return year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second 
-    }
 }
 var home = new Home();
 home.Init();
